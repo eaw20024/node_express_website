39d323e34a793dc0cf93ff8053b51347b19d1382
Eric Washington <Washington_Eric@bah.com>
2017-08-17 11:20:13 -0400

    Server.js File

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
+var expressLayouts = require('express-ejs-layouts');
+var bodyParser = require('body-parser');
 var app = express();
 var port = 8080;
+
+//use ejs and express layouts
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+
+//Use Body Parser 
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //Route my App folder
 var router = require('./app/routes');
